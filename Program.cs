758806aa0a83627d177f3e1fafcfa48eed8c7c1b
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2024-03-16 22:27:49 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Written for War Wind. https://store.steampowered.com/app/1741140/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BinaryReader br = new(File.OpenRead(args[0]));
+
+        List<int> offsets = [];
+        Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "//" + Path.GetExtension(args[0]));
+        int fileCount = br.ReadInt32();
+
+        for (int i = 0; i < fileCount; i++)
+            offsets.Add(br.ReadInt32());
+
+        offsets.Add((int)br.BaseStream.Length);
+
+        for (int i = 0; i < fileCount - 1; i++)
+        {
+            br.BaseStream.Position = offsets[i];
+            using FileStream FS = File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetExtension(args[0]) + "//" + i);
+            BinaryWriter bw = new(FS);
+            bw.Write(br.ReadBytes(offsets[i + 1] - offsets[i]));
+            bw.Close();
+        }
+    }
+}
